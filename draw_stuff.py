772e30f0e9fe9bd69f6359808381f82a1a8bab45
Jani Koskinen <koskinen.jani.p@gmail.com>
2024-05-03 18:31:14 +0300

    Added comments

--- draw_stuff.py
+++ draw_stuff.py
@@ -7,6 +7,9 @@ import numpy as np
 #%%
 
 def draw_route(coords, color='random', weight=7):
+    """
+    Draw folium polyline from a given list of coordinate pairs
+    """
     line = list(coords)
     if color == 'random':
          color = str(hex(np.random.randint(0,16777215)))
