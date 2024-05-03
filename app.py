f2d5477881970b17520d7657e6f15d233021bf6b
Jani Koskinen <koskinen.jani.p@gmail.com>
2024-05-03 18:00:28 +0300

    Minor edits

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from folium import Element
 import yaml
 from bs4 import BeautifulSoup
 from flask_session import Session
+import secrets
+# import redis
 
 import draw_stuff as ds
 
@@ -20,8 +22,9 @@ html = '''
     <h4>Mikä reitti tässä kulkee?</h4>
     <p> Kokeile, kuinka hyvin tunnet HSL:n joukkoliikenteen reitit </p>
     <p> Reittivaihtoehdot löytyvät pudotusvalikosta </p>
-    <p> Sovelluksen lähdekoodin löydät <a href="https://github.com/jpkos/routeguesr">sen Github-reposta</a>
-</div>
+    <p> Sovelluksen lähdekoodin löydät <a href="https://github.com/jpkos/routeguesr" target="_blank">sen Github-reposta</a>
+    <p> Virheilmoitukset ja parannusehdotukset: koskinen.jani.p [at) gmail.com </p>
+    </div>
 <script>
     var title = L.control({position: 'topleft'});
     title.onAdd = function (map) {
@@ -33,12 +36,14 @@ html = '''
 '''
 title_html = Element(html)
 app = Flask(__name__)
-app.secret_key = 'erittain_salainen'
+app.secret_key = '8db1eca554ed8f47a83cd9f22aa90efd'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-ZOOM_LVL = 11
+user_sessions = {}
+#%%
+ZOOM_LVL = 12
 LINE_COLOR = '#13B7FB'
 
 df_lines = pd.read_pickle('data/processed/lines.pkl').drop_duplicates(subset='line')
@@ -49,10 +54,10 @@ def index():
     # folium_map = folium.Map(location=start_coords,
     #                         zoom_start=11)
     # folium_map.get_root().html.add_child(title_html)
-    session['total_guesses'] = 0
-    session['correct_guesses'] = 0
+    user_sessions['total_guesses'] = 0
+    user_sessions['correct_guesses'] = 0
     random_line = df_lines.sample(1)
-    session['correct_line'] = random_line['line'].iloc[0]
+    user_sessions['correct_line'] = random_line['line'].iloc[0]
     coords = random_line['geometry'].iloc[0].coords
     start_coords = coords[0]
     folium_map = folium.Map(location=start_coords, zoom_start=ZOOM_LVL)
@@ -67,7 +72,7 @@ def index():
 @app.route('/new_line')
 def new_line():
     random_line = df_lines.sample(1).iloc[0]
-    session['correct_line'] = random_line['line']  # Store correct line in session
+    user_sessions['correct_line'] = random_line['line']  # Store correct line in session
 
     # Draw the line on the map
     coords = random_line['geometry'].coords
@@ -85,20 +90,26 @@ def new_line():
 @app.route('/check_guess', methods=['POST'])
 def check_guess():
     user_guess = request.form['guess']
-    correct_line = session.get('correct_line', 'Ei linjaa valittuna')  # Retrieve correct line from session
-    session['total_guesses'] += 1
+    correct_line = user_sessions.get('correct_line', 'Ei linjaa valittuna')  # Retrieve correct line from session
+    if 'total_guesses' not in user_sessions.keys():
+        user_sessions['total_guesses'] = 0
+    if 'correct_guesses' not in user_sessions.keys():
+        user_sessions['correct_guesses'] = 0
+    user_sessions['total_guesses'] += 1
     if user_guess == correct_line:
         result = f"Oikein! :) Oikea linja oli {correct_line}."
-        session['correct_guesses'] += 1
+        user_sessions['correct_guesses'] += 1
     else:
         result = f"Väärin :( Oikea linja oli {correct_line}."
 
     return jsonify(result=result,
-                   total_guesses=session['total_guesses'],
-                   correct_guesses=session['correct_guesses'])
+                   total_guesses=user_sessions['total_guesses'],
+                   correct_guesses=user_sessions['correct_guesses'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # port = int(os.getenv('PORT', 8080))
+    # app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=False)
 # %%
     # for i, df in df_lines.sample(5).groupby('line'):
     #     coords = df['geometry'].iloc[0].coords
