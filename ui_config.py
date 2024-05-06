a2526cfc7308d840ddec8d1a101aa867cd8ce9d6
Jani Koskinen <koskinen.jani.p@gmail.com>
2024-05-06 19:49:46 +0300

    Moved html banner from app

--- ui_config.py
+++ ui_config.py
@@ -1,2 +1,23 @@
+from folium import Element
+#%%
+html_banner = '''
+<div id="maptitle" style="position: fixed; top: 5%; left: 4%; width: 360px; z-index: 9999; font-size: 12px; border-radius: 5px; color: black; background-color: white; padding: 5px; box-shadow: 0 0 10px rgba(0,0,0,0.3);">
+    <h4>Mikä linja tässä kulkee?</h4>
+    <p> Kokeile, kuinka hyvin tunnet HSL:n joukkoliikenteen reitit ja linjat! </p>
+    <p> Sovelluksen lähdekoodin löydät <a href="https://github.com/jpkos/routeguesr" target="_blank">sen Github-reposta</a>
+    <p> Virheilmoitukset ja parannusehdotukset: </p>
+    <p> koskinen.jani.p [at) gmail.com </p>
+    </div>
+<script>
+    var title = L.control({position: 'topleft'});
+    title.onAdd = function (map) {
+        var div = L.DomUtil.get("maptitle");
+        return div;
+    };
+    title.addTo({{this}});
+</script>
+'''
+title_html = Element(html_banner)
+#%%
 ZOOM_LVL = 12
 LINE_COLOR = '#007AC9'
